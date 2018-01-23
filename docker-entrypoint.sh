f6d7ea2359e2491ced542e887fe07682aecdcddd
jamesalbert <jimmychanga.cheese@gmail.com>
2018-01-23 01:00:06 -0800

    mount repo as volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ -d /node_modules ]; then
+  mv /node_modules .
+fi
+
 sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' /usr/src/app/web/vue/UIconfig.js
-exec "$@"
+
+npm start
